1de8dcc700dd4413779777e6fc73eaae1fc0077f
Nathã Alves <nathaalvesaraujo@gmail.com>
2022-07-06 09:40:16 -0300

    App inicialization with GlobalStyles and react-router-dom

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyles from '../assets/styles/GlobalStyles'
+
+export default function App () {
+
+    return (
+        <>
+            <GlobalStyles />
+            <BrowserRouter>
+                <Routes>
+                    
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const element = document.querySelector('.root');
+ReactDOM.render(<App />, element);
