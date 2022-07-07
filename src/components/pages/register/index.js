eec2698d4c7a76210b89fddd7b5490b1309dc154
MariaDuda <mariabevenuto062@gmail.com>
2022-07-07 11:12:23 -0300

    get value from register screen

--- src/components/pages/register/index.js
+++ src/components/pages/register/index.js
@@ -1,17 +1,49 @@
 import styled from "styled-components";
 import { Link } from 'react-router-dom';
-import { RegisterGroup, RegisterTitle, RegisterInput,RegisterDivInput, RegisterSpan } from "./style.js"
+import { useState } from "react";
+import { RegisterGroup, RegisterTitle, RegisterInput, RegisterDivInput, RegisterSpan } from "./style.js"
 
 
 export default function Register() {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+    console.log(user.name)
     return (
         <RegisterGroup>
             <RegisterTitle>T3CH STORE</RegisterTitle>
             <RegisterDivInput>
-                <RegisterInput type="text" placeholder="nome"></RegisterInput>
-                <RegisterInput type="text" placeholder="email"></RegisterInput>
-                <RegisterInput type="text" placeholder="senha"></RegisterInput>
-                <RegisterInput type="text" placeholder="confirmar senha"></RegisterInput>
+                <RegisterInput
+                    type="text"
+                    placeholder="Nome"
+                    value={user.name}
+                    onChange={e => { setUser({ ...user, name: e.target.value }) }}>
+                </RegisterInput>
+
+                <RegisterInput
+                    type="text"
+                    placeholder="Email"
+                    value={user.email}
+                    onChange={e => { setUser({ ...user, email: e.target.value }) }}>
+                </RegisterInput>
+
+                <RegisterInput
+                    type="text"
+                    placeholder="Senha"
+                    value={user.password}
+                    onChange={e => { setUser({ ...user, password: e.target.value }) }}>
+                </RegisterInput>
+
+                <RegisterInput
+                    type="text"
+                    placeholder="confirmar senha"
+                    value={user.confirmPassword}
+                    onChange={e => { setUser({ ...user, confirmPassword: e.target.value }) }}>
+                </RegisterInput>
+
             </RegisterDivInput>
             <button type="submit" ><RegisterSpan>Cadastrar</RegisterSpan></button>
             <StyledLink to={"/"}>
